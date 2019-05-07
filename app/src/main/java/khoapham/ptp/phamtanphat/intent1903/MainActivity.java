ab934058892a97a6cfd499a1e90f16b48a7d7fae
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-07 19:58:59 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intent1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intent1903/MainActivity.java
@@ -2,12 +2,24 @@ package khoapham.ptp.phamtanphat.intent1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIntent = findViewById(R.id.buttonChuyenmanhinh);
+
+        btnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
